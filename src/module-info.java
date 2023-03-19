7c490918eb1d176442211ed1dd00693cb6ce0ed3
Bartek Łukawski <bwmn.peter@gmail.com>
2023-03-19 14:48:36 +0100

    Remove unused package

--- src/module-info.java
+++ src/module-info.java
@@ -1,11 +1,10 @@
 module org.wikipedia {
-	exports org.wikipedia;
-	exports org.wikipedia.tools;
-	exports org.wikipedia.servlets;
-	exports org.wikiutils;
+    exports org.wikipedia;
+    exports org.wikipedia.tools;
+    exports org.wikipedia.servlets;
+    exports org.wikiutils;
 
-	requires java.desktop;
-	requires java.logging;
-	requires java.net.http;
-	requires org.junit.jupiter.api;
+    requires java.desktop;
+    requires java.logging;
+    requires java.net.http;
 }
